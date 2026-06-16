c8ca259ae88a4f222e1082449f81690080f257dc
VobiCode <vobi@oncdev.ru>
2026-06-17 01:46:39 +0300

    upd

--- site/city/info_script.js
+++ site/city/info_script.js
@@ -1,3 +1,11 @@
+/**
+ * ПОДРОБНЕЕ В ./README-BACKEND.md
+ * ПОДРОБНЕЕ В ./README-BACKEND.md
+ * ПОДРОБНЕЕ В ./README-BACKEND.md
+ * ПОДРОБНЕЕ В ./README-BACKEND.md
+ * ПОДРОБНЕЕ В ./README-BACKEND.md
+*/
+/* 
 const API_KEY = 'AIzaSyBQ2q91BMaSt8f5OXHLAPX4FjBg7WxnSgY'; 
 const CHANNEL_ID = 'UCACwRQLU0Bq5yxijfnBPRfQ';
-
+*/
